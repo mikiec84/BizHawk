d81f028f5d733a1cbc4498cf77ca869f6a56266d
YoshiRulz <OSSYoshiRulz@gmail.com>
2019-06-19 23:13:26 +1000

    Change polar conversion in Virtual Pad to use lookup tables

--- BizHawk.Client.Common/input/PolarRectConversion.cs
+++ BizHawk.Client.Common/input/PolarRectConversion.cs
@@ -6,19 +6,8 @@ namespace BizHawk.Client.Common
 	{
 		/// <param name="θ">angle in degrees</param>
 		/// <returns>rectangular (Cartesian) coordinates (x, y)</returns>
-		public static Tuple<double, double> PolarDegToRect(double r, double θ) => PolarRadToRect(r, θ * Math.PI / 180);
-
-		/// <param name="θ">angle in radians</param>
-		/// <returns>rectangular (Cartesian) coordinates (x, y)</returns>
-		public static Tuple<double, double> PolarRadToRect(double r, double θ) => new Tuple<double, double>(r * Math.Cos(θ), r * Math.Sin(θ));
-
-		/// <param name="θ">angle in degrees</param>
-		/// <returns>rectangular (Cartesian) coordinates (x, y)</returns>
 		/// <seealso cref="RectToPolarLookup"/>
 		public static Tuple<short, short> PolarToRectLookup(ushort r, ushort θ) => new Tuple<short, short>(PolarRectConversionData._rθ2x.Value[r, θ], PolarRectConversionData._rθ2y.Value[r, θ]);
-
-		/// <returns>polar coordinates (r, θ) where θ is in degrees</returns>
-		public static Tuple<double, double> RectToPolarDeg(double x, double y) => new Tuple<double, double>(Math.Sqrt(x * x + y * y), Math.Atan2(y, x) * 180 / Math.PI);
 
 		/// <returns>polar coordinates (r, θ) where θ is in degrees</returns>
 		/// <remarks>does intentional integer (byte) overflow so <paramref name="x"/> and <paramref name="y"/> can be used as array indices in <see cref="TranslatedRectToPolarLookup"/></remarks>
